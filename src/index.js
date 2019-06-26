be743eae24e092eca4b594426906d7bb27a8d7ca
rajan-magar <52233171+rajan-magar@users.noreply.github.com>
2019-06-26 17:42:48 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div class='grid'>
+<div class='item'>
+  We use Parcel to bundle this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+<div class='item'>
+We use Parcel to bundle this sandbox, you can find more info about Parcel
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+</div>
+`;
